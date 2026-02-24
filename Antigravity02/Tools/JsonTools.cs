1e5514c40d783f7bfaf280e11efb1456e960a2a3
cm8 <cm8@winton.com.tw>
2026-02-24 16:36:53 +0800

    fix bug

--- Antigravity02/Tools/JsonTools.cs
+++ Antigravity02/Tools/JsonTools.cs
@@ -31,7 +31,42 @@ namespace Antigravity02.Tools
         public static T Deserialize<T>(string json)
         {
             if (string.IsNullOrEmpty(json)) return default;
+            
+            // 如果要解析為 Dictionary<string, object>，手動將 JToken 轉為舊式的 Dictionary 與 ArrayList 以相容原本代碼
+            if (typeof(T) == typeof(System.Collections.Generic.Dictionary<string, object>))
+            {
+                var jObject = Newtonsoft.Json.Linq.JObject.Parse(json);
+                return (T)(object)ToPrimitive(jObject);
+            }
+            
             return JsonConvert.DeserializeObject<T>(json);
+        }
+
+        private static object ToPrimitive(Newtonsoft.Json.Linq.JToken token)
+        {
+            if (token is Newtonsoft.Json.Linq.JObject jobj)
+            {
+                var dict = new System.Collections.Generic.Dictionary<string, object>();
+                foreach (var prop in jobj.Properties())
+                {
+                    dict[prop.Name] = ToPrimitive(prop.Value);
+                }
+                return dict;
+            }
+            if (token is Newtonsoft.Json.Linq.JArray jarr)
+            {
+                var list = new System.Collections.ArrayList();
+                foreach (var item in jarr)
+                {
+                    list.Add(ToPrimitive(item));
+                }
+                return list;
+            }
+            if (token is Newtonsoft.Json.Linq.JValue jval)
+            {
+                return jval.Value;
+            }
+            return null;
         }
     }
 }
